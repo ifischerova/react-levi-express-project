ef342f8a9d560ce798232b5175c2976901b2516a
Ivuska <fischerova.ivka@gmail.com>
2023-04-09 11:18:02 +0200

    Routing of sites, Reservation component with basic text added.

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,24 @@ import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from '../Reservation';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Home />,
+  },
+  {
+    path:'/reservation',
+    element: <Reservation />,
+  }
+]);
 
 export const App = () => (
   <>
     <Header />
-    <Home />
+    <RouterProvider router={ router }/>
     <Footer />
   </>
 );
---
+++ src/components/Reservation/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Reservation = () => {
+    return (
+        <h2>Detail jízdenky</h2>
+    );
+};
